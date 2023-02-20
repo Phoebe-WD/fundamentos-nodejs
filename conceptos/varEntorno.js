ebf9d2d75cf0b43cc4a8e00113ab9754c56e3bd3
Phoebe W <phoebe.wilckens@gmail.com>
2023-02-20 19:53:01 -0300

    feat: :sparkles: first commit
    first commit

---
+++ conceptos/varEntorno.js
@@ -0,0 +1,6 @@
+require("dotenv").config();
+let nombre = process.env.NOMBRE || "sin nombre";
+let web = process.env.WEB || "sin web";
+
+console.log("Hola " + nombre);
+console.log("Mi web es: " + web);
